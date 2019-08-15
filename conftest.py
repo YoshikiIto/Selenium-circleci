ab39bb050fffe41acc528710e7b9b0cda832063b
YoshikiIto <yoshikiito.el@gmail.com>
2019-08-15 15:19:24 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,11 +5,13 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
-    if report.when == 'call':
-        # always add url to report
-        extra.append(pytest_html.extras.url('http://www.example.com/'))
+    if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
-            # only add additional html on failure
-            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+            file_name = report.nodeid.replace("::", "_")+".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
         report.extra = extra
