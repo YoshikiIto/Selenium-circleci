02b2d6f3d588125b4da090ba17b4a9ae3375c113
YoshikiIto <yoshikiito.el@gmail.com>
2019-08-15 10:50:28 +0900

    失敗するテストを追加

--- test_google.py
+++ test_google.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
+
 class TestGoogle:
 
     def setup_class(cls):
@@ -17,16 +18,21 @@ class TestGoogle:
 
         driver.save_screenshot("test-reports/result_001.png")
 
-        driver.find_element_by_css_selector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input").send_keys("hoge")
-        driver.find_element_by_css_selector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input").send_keys(Keys.TAB)
+        driver.find_element_by_css_selector(
+            "#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input").send_keys("hoge")
+        driver.find_element_by_css_selector(
+            "#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input").send_keys(Keys.TAB)
 
         driver.save_screenshot("test-reports/result_002.png")
 
-        driver.find_element_by_css_selector("#tsf > div:nth-child(2) > div > div.FPdoLc.VlcLAe > center > input.gNO89b").click()
+        driver.find_element_by_css_selector(
+            "#tsf > div:nth-child(2) > div > div.FPdoLc.VlcLAe > center > input.gNO89b").click()
 
         driver.save_screenshot("test-reports/result_003.png")
 
         assert driver.title.count('hoge'), "ページタイトルにhogeが含まれていること"
+        assert driver.title.count(
+            'foobar'), "ページタイトルにfoobarが含まれていること"  # わざと失敗させる
 
     def teardown_class(cls):
         cls.driver.quit()
